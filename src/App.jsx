c4bdb530a85628c7cf7b47eb8706a31cb19806d6
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-03-15 13:29:33 +0100

    practicing axios

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FetchUserDetails from './Components/FetchUserDetails'
+
+const App = () => {
+  return (
+    <div>
+      <FetchUserDetails/>
+    </div>
+  )
+}
+
+export default App
